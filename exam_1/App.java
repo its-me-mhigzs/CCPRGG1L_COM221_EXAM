b5b91efd353dfea7544be04123bd07c9a8ab730b
Juan Miguel Andaya <111488270+mama-mo-github@users.noreply.github.com>
2022-09-08 14:28:16 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Student me = new Student("Andaya", "Juan Miguel", "Redulfin", "04/26/04", 2022101895, "andayajr@students.national-u.edu.ph", true);
+        me.sayMyStudentNumber() ;
+    
+    }
+}
